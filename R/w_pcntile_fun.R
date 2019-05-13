262406b0df4e8f2ffb6bc4dcefe9784305c7066e
MIRJAM ALLIK <mirjam.allik@gmail.com>
2019-05-13 11:28:13 +0100

    Added code to remove missing data from the deprivation variable and calculate percentiles only for areas for which the deprivation variable is present.
    Returns a vector of original length with NAs for percentile for those areas where the deprivation variable was missing.

--- R/w_pcntile_fun.R
+++ R/w_pcntile_fun.R
@@ -43,6 +43,15 @@ w_pcntile <- function(data, population, variable, p=10, low=F) {
   N <- length(df$population)
   df$index <- 1:N
 
+  # Remove missing data
+  if (any(is.na(df$variable))) {
+    has.missing <- TRUE
+    original.index <- df[, "index", drop = F]
+    df <- df[!is.na(df$variable), ]
+
+    # if ()... deal with missing population variable?
+  }
+
   # Order dataset
   df <- df[order(df$variable, decreasing=low), ]
 
@@ -53,7 +62,14 @@ w_pcntile <- function(data, population, variable, p=10, low=F) {
   # pcnt_loop is a c++ function within the package
   df$decile <- pcnt_loop(df$population, df$variable, p, cp)
 
+  if (has.missing) {
+    # add data to original index, thereby padding again with NA values
+    df <- merge(original.index, df, by = "index", all=T)
+
+  }
+
+  # order data by index
   df <- df[order(df$index),]
+
   return(df$decile)
 }
-
